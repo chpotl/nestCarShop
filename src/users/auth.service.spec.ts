5e72e2b578a3b8b6442e1059ff9b4460325aeda3
Leonid Zotov <zotovleonid@icloud.com>
2023-02-02 16:54:48 +0300

    add fakeUserService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,10 +1,25 @@
 import { Test } from '@nestjs/testing';
+import { create } from 'domain';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
+  // create fake userServise copy
+  const fakeUserService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email: 'a@a.com', password: '123' } as User),
+  };
+
   const module = await Test.createTestingModule({
-    providers: [AuthService],
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUserService,
+      },
+    ],
   }).compile();
 
   const service = module.get(AuthService);
